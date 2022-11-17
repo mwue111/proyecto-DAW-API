63cf11a102de6ef73d3b2331d65a78d9d6a9e7a7
MDW <mwue111@g.educaand.es>
2022-11-17 14:21:56 +0100

    tabla users + seeder

--- API/database/migrations/2014_10_12_000000_create_users_table.php
+++ API/database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,33 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration{
+    
+    public function up(){
+
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigIncrements('id')->index();
+            $table->string('username', 100)->unique();
+            $table->string('name', 100);
+            $table->string('surname1', 100);
+            $table->string('surname2', 100)->nullable();
+            $table->string('email', 50)->unique();
+            //$table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 50);
+            $table->date('birth_date');
+            $table->enum('type', ['customer', 'owner', 'admin']);
+            $table->string('image', 100);
+            //$table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down(){
+        
         Schema::dropIfExists('users');
     }
 };
---
+++ API/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersTableSeeder extends Seeder{
+
+    public function run(){
+        DB::table('users')->insert([
+            'username' => 'IvanPuga',
+            'name' => 'David',
+            'surname1' => 'Puga',
+            'surname2' => 'Ruano',
+            'email' => 'ivan@nombrereal.com',
+            'password' => 'password',
+            'birth_date' => '2020-09-03',
+            'type' => 'customer',
+            'image' => 'imagen falsa'
+        ]);
+        DB::table('users')->insert([
+            'username' => 'EmilioMartinez',
+            'name' => 'Emilio',
+            'surname1' => 'Martínez',
+            'surname2' => 'Sánchez',
+            'email' => 'emilio@nombrereal.com',
+            'password' => 'password',
+            'birth_date' => '1875-09-03',
+            'type' => 'admin',
+            'image' => 'imagen falsa'
+        ]);
+        DB::table('users')->insert([
+            'username' => 'juanLuis',
+            'name' => 'Juan Pedro',
+            'surname1' => 'Gimenez',
+            'surname2' => 'Picasso',
+            'email' => 'josealberto@nombrereal.com',
+            'password' => 'password',
+            'birth_date' => '1794-09-03',
+            'type' => 'admin',
+            'image' => 'imagen falsa'
+        ]);
+        DB::table('users')->insert([
+            'username' => 'ari',
+            'name' => 'Irene',
+            'surname1' => 'Montero',
+            'surname2' => 'Ayuso',
+            'email' => 'ari@nombrereal.com',
+            'password' => 'password',
+            'birth_date' => '1955-09-03',
+            'type' => 'admin',
+            'image' => 'imagen falsa'
+        ]);
+        DB::table('users')->insert([
+            'username' => 'jackieChan',
+            'name' => 'Quian',
+            'surname1' => 'Martínez',
+            'surname2' => 'Montero',
+            'email' => 'chanchitofeliz@nombrereal.com',
+            'password' => 'password',
+            'birth_date' => '2020-09-03',
+            'type' => 'owner',
+            'image' => 'imagen falsa'
+        ]);
+    }
+}
