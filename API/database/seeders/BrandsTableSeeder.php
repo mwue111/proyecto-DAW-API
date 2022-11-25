2e15f1349b145769839fb8aaa007c7c4210dcfa0
Enrique <emarsan708@g.educaand.es>
2022-11-25 13:26:54 +0100

    brands añadida

--- API/database/seeders/BrandsTableSeeder.php
+++ API/database/seeders/BrandsTableSeeder.php
@@ -14,6 +14,17 @@ class BrandsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('brands')->insert([
+            'name' => 'CASI',
+            'description' => 'Cooperativa Agrícola San Isidro',
+        ]);
+        DB::table('brands')->insert([
+            'name' => 'Juver',
+            'description' => 'COmpañía líder en los zumos de frutas y verduras',
+        ]);
+        DB::table('brands')->insert([
+            'name' => 'Danone',
+            'description' => 'Lácteos de la mejor calidad',
+        ]);
     }
 }
