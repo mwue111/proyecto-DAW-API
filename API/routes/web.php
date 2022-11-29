5b45708ca8e03c19dbe5bc6caaa3683e7eb7eb39
JuanDiegoJimenez <jjimpic941@g.educaand.es>
2022-11-29 15:29:51 +0100

    Route de Address, Town y State

--- API/routes/web.php
+++ API/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('addresses', 'AddressController');
+Route::resource('towns', 'TownController');
+Route::resource('states', 'StateController');
