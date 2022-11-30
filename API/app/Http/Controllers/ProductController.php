04380445e01d80e1b0ae9db8a05bb78a9294412c
Enrique <emarsan708@g.educaand.es>
2022-11-30 08:49:28 +0100

    product show test

--- API/app/Http/Controllers/ProductController.php
+++ API/app/Http/Controllers/ProductController.php
@@ -24,7 +24,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
@@ -46,7 +46,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        return Product::find($id);
     }
 
     /**
