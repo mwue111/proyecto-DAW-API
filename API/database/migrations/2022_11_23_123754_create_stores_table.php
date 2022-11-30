a9b42c0c194d8fc8f85d4917bc89c7ce65e93e55
JuanDiegoJimenez <jjimpic941@g.educaand.es>
2022-11-30 17:39:43 +0100

    timestamps agregados a las tablas

--- API/database/migrations/2022_11_23_123754_create_stores_table.php
+++ API/database/migrations/2022_11_23_123754_create_stores_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->double('latitude');
             $table->integer('store_hours_id');
             $table->integer('special_days_id');
+            $table->timestamps();
         });
     }
 
