e2721bdb62849f0a84b427bd52fcd9c8f9d3277b
mwue111 <mwue111@g.educaand.es>
2022-12-01 13:27:47 +0100

    Actualizado DatabaseSeeder

--- API/database/seeders/DatabaseSeeder.php
+++ API/database/seeders/DatabaseSeeder.php
@@ -31,6 +31,18 @@ class DatabaseSeeder extends Seeder
         $this->call(ClientsTableSeeder::class);
         $this->call(StatesTableSeeder::class);
         $this->call(TownsTableSeeder::class);
+        //Tablas de Dany:
+        $this->call(FilesTableSeeder::class);
+        $this->call(Stores_imgTableSeeder::class);
+        $this->call(DocumentsTableSeeder::class);
+        $this->call(Products_imgTableSeeder::class);
+        $this->call(Profiles_imgTableSeeder::class);
+        $this->call(Brands_imgTableSeeder::class);
+        $this->call(AddressTableSeeder::class);
+        $this->call(SchedulesTableSeeder::class);
+        $this->call(TimeslotsTableSeeder::class);
+        $this->call(Products_storesTableSeeder::class);
+        $this->call(SalesTableSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
