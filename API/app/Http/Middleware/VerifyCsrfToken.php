d720d2b7b6753bd56fa6daebda56a98b09b49884
JuanDiegoJimenez <jjimpic941@g.educaand.es>
2022-12-02 12:08:21 +0100

    CRUD file sin relación

--- API/app/Http/Middleware/VerifyCsrfToken.php
+++ API/app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
         "categoria*",
         "etiqueta*",
         "provincia*",
-        "documento*"
+        "documento*",
+        "archivo*"
     ];
 }
