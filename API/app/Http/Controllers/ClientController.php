f2249ffb13754e4a816e15ea283118f3d43de268
mwue111 <mwue111@g.educaand.es>
2022-12-12 10:13:24 +0100

    Descomentado un método en controlador cliente

--- API/app/Http/Controllers/ClientController.php
+++ API/app/Http/Controllers/ClientController.php
@@ -15,12 +15,12 @@ class ClientController extends Controller{
     public function store(Request $request){
         $client = Client::create($request->all());
     }
+    */
     
     //mostrar el cliente de un id concreto y sus datos como usuario
     public function show($id){
         $client = Client::find($id);
         $client->user;
         return $client;
-    }
-    */
+    }   
 }
