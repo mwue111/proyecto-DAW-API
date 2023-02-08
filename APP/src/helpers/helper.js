208e2a1c2d0aeb8ae749144ea5a828b58d96329f
mwue111 <mwue111@g.educaand.es>
2023-02-08 18:01:13 +0100

    eliminadas funciones repetidas helper

--- APP/src/helpers/helper.js
+++ APP/src/helpers/helper.js
@@ -40,6 +40,8 @@ function formatJsonTienda (tiendas){
             descripcion: item.description,
             address: item.address,
             schedules: item.schedules,
+            deleted: item.deleted,
+            updated_at: item.updated_at
         }
     })
     return data;
@@ -129,69 +131,6 @@ function headersDB(oldHeaders){
 
 export function objectProfoundCopy(object){
     return JSON.parse(JSON.stringify(object));
-}
-///////////////////////////////////////////
-// TIENDA
-///////////////////////////////////////////
-function formatJsonTienda (tiendas){
-    const data = tiendas.map((item) => {
-        return {
-            id: item.id,
-            nombre: item.name,
-            direccion: formatJsonDireccion(item.address),
-            telefono1: item.telephone1,
-            telefono2: item.telephone2,
-            email: item.email,
-            propietario: item.owner.user.username,
-            horario: formatJsonHorario(item.schedules),
-            descripcion: item.description,
-            address: item.address,
-            schedules: item.schedules,
-
-        }
-    })
-    return data;
-}
-
-function formatJsonDireccion (address){
-    return address.road_type + " " + address.name + " " + address.number + ", " + address.zip_code + " " + address.town.name + " (" + address.town.state.name.toUpperCase() + ")";
-}
-
-function formatJsonHorario (schedules){
-    let horario = '';
-    schedules.map((item) => {
-        horario += formatJsonDia(item.day_of_week) + ": " + item.time_slot.open_time + " - " + item.time_slot.closed_time + " \n";
-    })
-    return horario;
-}
-function formatJsonDia(day){
-    let dia="";
-    switch (day) {
-        case 1:
-            dia="Lunes";
-            break;
-        case 2:
-            dia="Martes";
-            break;
-        case 3:
-            dia="Miércoles";
-            break;
-        case 4:
-            dia="Jueves";
-            break;
-        case 5:
-            dia="Viernes";
-            break;
-        case 6:
-            dia="Sábado";
-            break;
-        case 7:
-            dia="Domingo";
-            break;
-        default:
-            break;
-    }
-    return dia;
 }
 
 ///////////////////////////////////////////
